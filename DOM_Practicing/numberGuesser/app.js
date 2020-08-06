0249bde1b5404d0ac66d1d164bbe4476257233bb
Hassan Dayem <hassandayem91@gmail.com>
2020-08-06 10:00:46 +0300

    enhanced the game and final touches

--- DOM_Practicing/numberGuesser/app.js
+++ DOM_Practicing/numberGuesser/app.js
@@ -2,7 +2,7 @@
 GAME RULES
 1. The user should enter a number between min and max
 2. If the number was equal to winningNumb, notify the user (WIN) and disable the input.
-3. Esle show the message (LOSE), and notify user.
+3. Else show the message (LOSE), and notify user.
 4. Allow the user to play again according to guessesLeft value.
 5. If guessesLeft expired, notify the user and display what was the winningNumb.
 6. Show a button in place of submit, says 'play again'.
@@ -11,7 +11,7 @@ GAME RULES
 // Define the game variables
 let min = 1,
   max = 10,
-  winningNumb = 2,
+  winningNumb = getRandomNum(min, max),
   guessesLeft = 3;
 
 // The UI
@@ -25,6 +25,13 @@ const game = document.getElementById("game"),
 // Listen to the submit button
 submitBtn.addEventListener("click", validateGame);
 
+// Listen to play again button
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
 // Validate Game function
 function validateGame() {
   let inputValue = parseInt(guessInput.value);
@@ -33,32 +40,57 @@ function validateGame() {
   if (isNaN(inputValue) || inputValue < min || inputValue > max) {
     dynamicMessage(
       `Sorry, you have to enter a number between ${min} and ${max}`,
-      "red"
+      "orange"
     );
     // Change the border color, to improve the Mr. user experience
-    guessInput.style.borderColor = "red";
+    guessInput.style.borderColor = "orange";
   }
 
-  // Check if Won
+  // Check if the game over
   if (inputValue === winningNumb) {
-    // Make the user happy
-    dynamicMessage(
-      `CONGRATULATIONS! you win, ${winningNumb} is correct.`,
-      "green"
-    );
-    // Change the border color
-    guessInput.style.borderColor = "green";
-    // Disable the input field
-    guessInput.disabled = true;
+    // Game Over - WON - Make the user happy
+    gameOver(true, `CONGRATULATIONS! you win, ${winningNumb} is correct.`);
   } else {
-    // Make the user angyry
-    dynamicMessage(
-      `Sorry, that was wrong but you still have ${guessesLeft} guesses left`,
-      "red"
-    );
-    // Change the border color
-    guessInput.style.borderColor = "red";
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // Game Over -LOST- Make the user angry
+      gameOver(false, `Sorry, you lost, the correct number was ${winningNumb}`);
+    } else {
+      // Give the user some hope
+      dynamicMessage(
+        `Sorry, that was wrong but you still have ${guessesLeft} guesses left`,
+        "red"
+      );
+      // Change the border color
+      guessInput.style.borderColor = "red";
+      // Empty the field to enter a new value
+      guessInput.value = "";
+    }
   }
+}
+
+// Game over
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  // Change the border color
+  guessInput.style.borderColor = color;
+  // Change the message color
+  message.style.color = color;
+  // Disable the input field
+  guessInput.disabled = true;
+  // Set the message
+  dynamicMessage(msg);
+
+  // Play Again
+  submitBtn.value = "Play Again";
+  submitBtn.className += "play-again";
+}
+
+// Get a random number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // message function
