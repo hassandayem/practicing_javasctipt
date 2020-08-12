ac10e371e7a4751991a53ad79f603f7a1e8f51a2
Hassan Dayem <hassandayem91@gmail.com>
2020-08-12 09:55:50 +0300

    added the reamining and subtract features

--- DOM_Practicing/small-budget-app/app.js
+++ DOM_Practicing/small-budget-app/app.js
@@ -1,23 +1,70 @@
 // Declare UI variables
+const UIbudgetForm = document.querySelector(".budget__form");
 const UIexpenseForm = document.querySelector(".expense__form");
-const UIepxenseInput = document.querySelector(".expense__input");
-const UIbudgetRemaining = document.querySelector(".budget__remaining");
 const UIepxenseAmount = document.querySelector(".expense__form--amount");
 const UIepxenseCategory = document.querySelector(".expense__form--category");
+const UIepxenseInput = document.querySelector(".budget__input");
+const UIexpenseSubmitBtn = document.querySelector(".expense__submit");
+const UIbudgetRemaining = document.querySelector(".budget__remaining");
+// The Table
+const table = document.querySelector(".table");
 
-console.log(parseInt(UIbudgetRemaining.textContent));
-let budgetValue = parseInt(UIbudgetRemaining.textContent);
+let budgetRemainingValue = parseInt(UIbudgetRemaining.textContent);
 
 // Add the daily budget - functionality
-const addExpense = (e) => {
-  let expenseValue = parseInt(e.target.querySelector("input").value);
-  budgetValue += expenseValue;
-  UIbudgetRemaining.innerHTML = `<h3 class="display-3">${budgetValue}$</h3>`;
+const addBudget = (e) => {
+  let budgetValue = parseInt(e.target.querySelector("input").value);
+  // Change the budget value to equal expense value
+  budgetRemainingValue += budgetValue;
+  // Reflect the change on the UI
+  UIbudgetRemaining.innerHTML = `<h3 class="display-3">${budgetRemainingValue}$</h3>`;
+  // Reset the field after submit
   UIepxenseInput.value = "";
+  // Blur the field after submit
   UIepxenseInput.blur();
 
   e.preventDefault();
 };
 
-// Add the daily budget - listen to submit event
-UIexpenseForm.addEventListener("submit", addExpense);
+// Subtract budget from amount
+function subtractBudget(e) {
+  // Grab the value of expense input
+  let expenseValue = parseInt(UIepxenseAmount.value);
+  // Subtract from budget value
+  budgetRemainingValue -= expenseValue;
+  // Reflect changes on UI
+  UIbudgetRemaining.innerHTML = `<h3 class="display-3">${budgetRemainingValue}$</h3>`;
+  // Reset the amount field after submition
+  UIepxenseAmount.value = "";
+
+  addToTable();
+
+  e.preventDefault();
+}
+
+// Listen to budget submit event - Add the daily budget
+UIbudgetForm.addEventListener("submit", addBudget);
+
+// Listen to the add amount click event - Submit the expense with amount and category
+UIexpenseSubmitBtn.addEventListener("click", subtractBudget);
+
+// Add details to the table
+function addToTable() {
+  console.log(UIepxenseCategory.value);
+
+  // Dealing with table
+  const tHeads = table.tHead.getElementsByTagName("th");
+
+  // Create new row
+  let newRow = table.insertRow(-1);
+  // Create new cell
+  let newCell = newRow.insertCell(0);
+  // Append a text to cell
+  let newText = document.createTextNode(UIepxenseCategory.value);
+  newCell.appendChild(newText);
+  let cost = document.createTextNode(UIepxenseAmount.value);
+  newCell.appendChild(cost);
+
+  // Reset the category field after submition
+  UIepxenseCategory.value = "";
+}
